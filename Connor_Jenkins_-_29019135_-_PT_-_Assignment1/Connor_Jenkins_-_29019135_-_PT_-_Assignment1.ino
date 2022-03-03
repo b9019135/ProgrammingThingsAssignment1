fed04b6597623fc1c62268f76c7b63bdb0e43504
b9019135 <74562468+b9019135@users.noreply.github.com>
2022-03-03 01:44:41 +0000

    update 1.2

--- Connor_Jenkins_-_29019135_-_PT_-_Assignment1/Connor_Jenkins_-_29019135_-_PT_-_Assignment1.ino
+++ Connor_Jenkins_-_29019135_-_PT_-_Assignment1/Connor_Jenkins_-_29019135_-_PT_-_Assignment1.ino
@@ -1,57 +1,129 @@
 #include <Zumo32U4.h>
-
+#include <Wire.h>
+#include <PololuBuzzer.h>
 
 Zumo32U4Motors motors;
+Zumo32U4LineSensors linedetection;
+Zumo32U4Buzzer buzzer;
+
+#define QTR_THRESHOLD 700 // Microseconds (Change depending on lighting and surface conditions)
+
+#define numberofsensors 3
+unsigned int linesensorvalues[numberofsensors];
 
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial1.begin(9600);
-  Serial.begin(9600);
+  linedetection.initThreeSensors();
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
-  int incomingcommand =0; //Data incoming from serial.
+void automatic()
+{
+  linedetection.read(linesensorvalues);
 
-  if (Serial1.available() > 0) {
+if(linesensorvalues[0] > QTR_THRESHOLD) //left sensor
+{
+ // buzzer.playNote(NOTE_A(4), 2000, 10);
+  reverse();
+  delay(300);
+  right();
+  delay(300);
+  forward();
+}
+else if(linesensorvalues[numberofsensors -1] > QTR_THRESHOLD) //right sensor
+{
+  //buzzer.playNote(NOTE_B(4), 1000, 10);
+  reverse();
+  delay(300);
+  left();
+  delay(100);
+  forward();
+}
+else
+{
+  forward();
+  delay(2);
+}
 
-    incomingcommand = Serial1.read();
+}
 
-    switch(incomingcommand) {
-      case 'f':
+void forward()
+{
+      Serial1.println("Moving forward");
       motors.setLeftSpeed(100);
       motors.setRightSpeed(100);
       delay(2);
-      break;
-      case 'b':
+}
+
+void reverse()
+{
+      Serial1.println("Moving backwards");
       motors.setLeftSpeed(-100);
       motors.setRightSpeed(-100);
-      delay(5);
+      delay(2);
+}
+
+void left()
+{
+      Serial1.println("Turn left");
+      motors.setLeftSpeed(-100);
+      motors.setRightSpeed(100);
+      delay(2);
+}
+void right()
+{
+      Serial1.println("Turn right");
+      motors.setLeftSpeed(100);
+      motors.setRightSpeed(-100);
+      delay(2);
+}
+
+void hault()
+{
+  buzzer.playNote(NOTE_C(4), 1000, 10);
+  Serial1.println("Stopped");
+  motors.setLeftSpeed(0);
+  motors.setRightSpeed(0);
+  delay(2);
+}
+
+void commands(int movecommand)
+{
+    switch(movecommand) {
+      case 'f':
+      forward();
+      break;
+      case 'b':
+      reverse();
       break;
       case 'l':
-      motors.setLeftSpeed(100);
-      motors.setRightSpeed(0);
-      delay(5);
+      left();
       break;
       case 'r':
-      motors.setLeftSpeed(0);
-      motors.setRightSpeed(100);
-      delay(5);
+      right();
       break;
       case 's':
-      motors.setLeftSpeed(0);
-      motors.setRightSpeed(0);
-      delay(5);
+      hault();
+      break;
+      case 'a':
+      do{automatic();}while(movecommand != 's');
       break;
     }
+}
 
 
-  }
-    
-  }
+void loop() {
+  
+  int incomingcommand =0; //Data incoming from serial.
+
+  if (Serial1.available() > 0) {
+    incomingcommand = Serial1.read();
+    commands(incomingcommand);
+}
+}
 
  
